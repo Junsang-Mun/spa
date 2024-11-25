20287604c6c3574831a08b695c12059c768045fc
Junsang Mun <mun.js@aol.com>
2024-11-25 12:49:04 +0900

    chroe: 이벤트리스너 생성 및 navigate 생성
    - totally not a game 및 scoreboard에 신규 생성

--- src/pages/main.js
+++ src/pages/main.js
@@ -7,8 +7,8 @@ export function render(app, navigate) {
 			<div class="grid-item-right" id="online-4p">온라인에서 4명이서 싸우기</div>
 			<div class="grid-item-left" id="offline-2p">오프라인으로 맞짱까기</div>
 			<div class="grid-item-right" id="offline-ai">AI를 부수기</div>
-			<div class="grid-item-left" id="offline-2p">스코어보드</div>
-			<div class="grid-item-right" id="offline-ai">토탈리낫어게임</div>
+			<div class="grid-item-left" id="scoreboard">스코어보드</div>
+			<div class="grid-item-right" id="totallynotagame">토탈리낫어게임</div>
 		  </div>
 	  `;
   
@@ -16,4 +16,6 @@ export function render(app, navigate) {
 	document.getElementById('online-4p').addEventListener('click', () => navigate('game/online/4p'));
 	document.getElementById('offline-2p').addEventListener('click', () => navigate('game/offline/2p'));
 	document.getElementById('offline-ai').addEventListener('click', () => navigate('game/offline/ai'));
+	document.getElementById('scoreboard').addEventListener('click', () => navigate('scoreboard'));
+	document.getElementById('totallynotagame').addEventListener('click', () => navigate('totallynotagame'));
 }
