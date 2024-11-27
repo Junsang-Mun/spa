d2e4375d89786243c2cc18f5abdc89c2d23797e6
Junsang Mun <mun.js@aol.com>
2024-11-27 17:05:05 +0900

    chroe: minor changes on locale changer

--- src/modules/header.js
+++ src/modules/header.js
@@ -2,11 +2,11 @@ export function renderHeader(header) {
     header.innerHTML = `
         <header class="header">
             <h1>PONG</h1>
-            <div class="dropbtn" id="dropbtn">Language</div>
+            <button class="dropbtn" id="dropbtn">Language</button>
             <div class="dropdown-content" id="dropdowncontent">
-                <a href="#" id="en-US">English</a>
-                <a href="#" id="ja-JP">日本語</a>
-                <a href="#" id="ko-KR">한국어</a>
+                <button id="en-US">English</button>
+                <button id="ja-JP">日本語</button>
+                <button id="ko-KR">한국어</button>
             </div> <!-- .dropdown-content -->
         </header>
     `;
