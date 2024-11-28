187572b9b504f4de859b2d699587d9d17cba6f39
Junsang Mun <mun.js@aol.com>
2024-11-28 10:12:41 +0900

    chroe: remove legacy in error.js
    - t에서 더이상 locale 값이 사용되지 않는데 남아있던 것 해결

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,11 +1,11 @@
 import { t } from '/src/modules/locale/localeManager.js';
 
-export function render(app, navigate, locale = 'ja-JP') {
+export function render(app, navigate) {
     app.innerHTML = `
     <div style="background-color: #0078D7; padding: 2rem">
         <h1 style="text-align: left">:(</h1>
-        <p style="text-align: left">${t('error', 'sorry', locale)}</p>
-        <button class="back-button" id="backButton">${t('errorButton', 'back', locale)}</button>
+        <p style="text-align: left">${t('error', 'sorry')}</p>
+        <button class="back-button" id="backButton">${t('errorButton', 'back')}</button>
     </div>
     `;
     console.log('⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣦⠀\n⠀⠀⠀⠀⣰⣿⡟⢻⣿⡟⢻⣧\n⠀⠀⠀⣰⣿⣿⣇⣸⣿⣇⣸⣿\n⠀⠀⣴⣿⣿⣿⣿⠟⢻⣿⣿⣿\n⣠⣾⣿⣿⣿⣿⣿⣤⣼⣿⣿⠇\n⢿⡿⢿⣿⣿⣿⣿⣿⣿⣿⡿⠀\n⠀⠀⠈⠿⠿⠋⠙⢿⣿⡿⠁⠀');
