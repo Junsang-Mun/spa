2b65b99e6d356e64a8790db4d7e91b1fcb83aa4c
Junsang Mun <mun.js@aol.com>
2024-12-06 16:47:02 +0900

    feat: 전적 기록 화면 추가

--- src/pages/log/main.js
+++ src/pages/log/main.js
@@ -3,9 +3,12 @@ import { t } from '/src/modules/locale/localeManager.js';
 export function render(app, navigate) {
 	app.innerHTML = `
 		<div class="grid">
-			<div class="grid-item-left" id="online-2p">${t('result-t', 'tournament match log')}</div>
-			<div class="grid-item-right" id="online-4p">${t('result-d', 'dual match log')}</div>
-			<div class="grid-item-left" id="offline-2p">${t('result-rps', 'RSP match log')}</div>
+			<div class="grid-item-left" id="result-t">${t('result-t', 'tournament match log')}</div>
+			<div class="grid-item-right" id="result-d">${t('result-d', 'dual match log')}</div>
+			<div class="grid-item-left" id="result-rps">${t('result-rps', 'RSP match log')}</div>
 		</div>
 	`;
+	document.getElementById('result-t').addEventListener('click', () => navigate('log/tournament'));
+	document.getElementById('result-d').addEventListener('click', () => navigate('log/dual'));
+	document.getElementById('result-rps').addEventListener('click', () => navigate('log/rps'));
 }
